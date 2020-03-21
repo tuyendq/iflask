f4207365f82a1c62f96eff031114256958e4f5b8
Tuyen Dang <tuyendq@gmail.com>
2020-03-21 23:06:35 +0700

    routes.py: return html page

--- app/routes.py
+++ app/routes.py
@@ -3,5 +3,14 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-    print("Handling request from home page.")
-    return "Hello Azure! First Python webapp with Flask."
+    print("Return HTML page.")
+    user = {'username': 'Tuyen'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
