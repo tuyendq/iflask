9738dc73fd83c7eb1194809eb145b2b94013cdf5
Tuyen Dang <tuyendq@gmail.com>
2020-03-22 21:35:09 +0700

    add posts list

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,14 @@ from app import app
 def index():
     print("Return HTML page using template.")
     user = {'username': 'Practice Habits'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]    
+    return render_template('index.html', title='Home', user=user, posts=posts)
